35eb5ab03688a946c48097b0c59de417638f37c8
Dan Garro <dobetterwithdan@gmail.com>
2022-12-05 14:30:02 -0500

    simple manatee jokes app in kivy

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivymd.uix.label import MDLabel
 
 import config
 import requests
+import json
 
 
 class ManateeApp(MDApp):
@@ -14,3 +15,20 @@ class ManateeApp(MDApp):
         self.theme_cls.material_style = "M3"
 
         return Builder.load_file("manatee.kv")
+
+    def jokes(self):
+        """Access Manatee Jokes API and display joke"""
+        self.root.ids.man_jokes.text = ""
+        url = config.api_url
+        headers = {"X-RapidAPI-Key": config.api_key, "X-RapidAPI-Host": config.api_host}
+        response = requests.request("GET", url, headers=headers)
+        jokes = response.json()
+        setup = jokes["setup"]
+        joke = jokes["punchline"]
+        self.root.ids.man_jokes.add_widget(
+            MDLabel(text=f"Setup: {setup}\nJoke: {joke}")
+        )
+
+
+if __name__ == "__main__":
+    ManateeApp().run()
